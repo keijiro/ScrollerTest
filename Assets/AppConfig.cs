0845b8ab6ee59cf4f351878563b26e240e46676a
Keijiro Takahashi <keijiro@Odyssey.jt.unity3d.com>
2015-04-23 14:56:00 +0900

    Initial commit.

---
+++ Assets/AppConfig.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppConfig : MonoBehaviour
+{
+    static int mode;
+
+    public static float Delta {
+        get {
+            switch (mode / 3) {
+                case 0: return Time.deltaTime;
+                case 1: return Time.smoothDeltaTime;
+                default: return 1.0f / 60;
+            }
+        }
+    }
+
+    public static int Layers {
+        get { return mode % 3; }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            mode = (mode + 1) % 9;
+    }
+
+    void OnGUI()
+    {
+        string[] modeNames = {"deltaTime", "smoothDeltaTime", "1/60s"};
+        GUI.Label(new Rect(0, 0, 200, 100), modeNames[mode / 3]);
+    }
+}
---
+++ Assets/Scroller.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroller : MonoBehaviour
+{
+    public float scrollSpeed = 1;
+    public float repeatPoint = 1;
+    public int layer;
+
+    float position;
+
+    void Start()
+    {
+        position = -transform.position.y;
+    }
+
+    void Update()
+    {
+        var flag = layer <= AppConfig.Layers;
+
+        GetComponent<Renderer>().enabled = flag;
+
+        if (flag)
+        {
+            position += AppConfig.Delta * scrollSpeed;
+
+            if (position > repeatPoint) position -= repeatPoint * 2;
+
+            var p = transform.position;
+            p.y = -position;
+            transform.position = p;
+        }
+    }
+}
